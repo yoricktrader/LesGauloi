d6ca963bfb17b85e11830f21f930b0a05eaac162
yoricktrader <djameleddine.bouzidi.fac@gmail.com>
2024-02-15 11:59:43 +0100

    TP1 classes

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,43 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	private void parler(String texte) {
+		System.out.println(prendreParole() + "<<" + texte + ">>");
+	}
+
+	private String prendreParole() {
+		return "Le gaulois" + nom + " : ";
+	}
+
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+		romain.recevoirCoup(force / 3);
+	}
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+	
+	public static void main(String[] args) {
+		Romain mero = new Romain("Mero", 3);
+		Gaulois asterix = new Gaulois("asterix", 8);
+		asterix.prendreParole();
+		asterix.parler("Bonjour");
+		asterix.frapper(mero);
+	}
+}
