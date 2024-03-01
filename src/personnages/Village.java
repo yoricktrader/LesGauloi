91e1d07db7a966d43d1b22414832f52651cd6a0e
yoricktrader <djameleddine.bouzidi.fac@gmail.com>
2024-03-01 09:29:39 +0100

    Ajout attribut, methode et main dans classe Village

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -2,12 +2,50 @@ package personnages;
 
 public class Village {
 	private String nom;
+	private Gaulois[] villageois ;
+	private int nbVillageois = 0;
+	private int nbVillageoisMaximum;
 
-	public Village(String nom) {
+	public village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
+		this.nbVillageoisMaximum = nbVillageoisMaximum;
+		this.villageaois = new Gaulois[nbVillageoisMaximum];
 	}
 
 	public String getNom() {
 		return nom;
 	}
+	
+	public ajouterHabitant(Gaulois nvhabitant) {
+		if this.nbVillageois >= this.nbVillageoisMaximum{
+			System.out.println(" Pas de place ");
+		}
+		else {
+			this.nbVillageois = this.nbVillageois + 1;
+			villageaois[this.nbvillageaois]= nvhabitant;
+		}
+	}
+	
+	public trouverHabitant(int indice) {
+		return villageois[indice];
+	}
+	
+	public void afficherVillageois(Village village) {
+
+        for (int i = 1; i < nbVillageois; i++) {
+        	if village.villageois[i] == Gaulois {System.out.println("Villageois : " + village.villageois[i].getNom());}
+        	else {System.out.println("Chef du village : " + village.villageois[i].getNom());}
+        }
+    }
+}
+	
+	public static void main(String[] args) {
+		Village villages = new Village("Village des Irreductibles");
+		village(vilage.nom, 30);
+		Chef abraracourcix = new Chef("Abraracourcix",6,villages);
+		Gaulois asterix = new Gaulois("Asterix",8,villages);
+		Gaulois oblixe = new Gaulois("Obleixe",25,villages);
+		afficherVillageois(villages);
+	}
+	
 }
